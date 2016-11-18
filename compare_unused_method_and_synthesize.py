6b58f82533eb49f6fdeec9b3e142cd0e86b86ba3
徐家伟 <xujiawei@autohome.com.cn>
2016-11-18 16:59:18 +0800

    change to smtp send email

--- compare_unused_method_and_synthesize.py
+++ compare_unused_method_and_synthesize.py
@@ -1,34 +1,23 @@
-import xlrd
-import xlwt
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-book = xlrd.open_workbook("Workbook1.xlsx")
+# addressbook list you will send to
+address_book = ['xxx@xxx.com']
+msg = MIMEMultipart()
+# sender
+sender = 'xxx@xxx.com'
+subject = "subject"
+body = "email body"
 
-print "the number of worksheet is {0}".format(book.nsheets)
+msg['From'] = sender
+msg['To'] = ','.join(address_book)
+msg['Subject'] = subject
+msg.attach(MIMEText(body, 'plain'))
+text=msg.as_string()
 
-print "worksheet name: {0}".format(book.sheet_names())
-
-sh = book.sheet_by_index(0)
-
-print "{0}  {1} {2}".format(sh.name, sh.nrows, sh.ncols)
-
-for rowindex in range(0, sh.nrows):
-	for column_value in sh.row_values(rowindex):
-		print column_value
-
-# print "Cell D2 is {0}".format(sh.cell_value(rowx=2, colx=3))
-
-# workbook = xlwt.Workbook()
-
-# sheet = workbook.add_sheet('Sheettttt')
-
-# toWrite = {
-# 	"a" : "b",
-# 	"c" : "d",
-# 	"e" : "f"
-# }
-
-# for index, value in enumerate(toWrite):
-# 	sheet.write(0, index, value)
-
-# workbook.save("XLWT.xls")
-
+s = smtplib.SMTP('microsolf_exchange_domain', 587)
+s.starttls()
+s.login('USERNAME','PASSWORD')
+s.sendmail(sender,address_book, text)
+s.quit()
